c89cb013d27795831e0cb780ccfb24cecb00d5c6
rahul941999 <83701790+rahul941999@users.noreply.github.com>
2023-10-21 00:16:11 +0530

    tidy up the page.

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ with col2:
      a reputed organisation as a python developer.
     """
     st.info(content)
+st.divider()
 st.text('Below you can find some of the app that i have built '
         'in Python. Feel free to contact me!')
-col3, col4 = st.columns(2)
+st.divider()
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 data = pd.read_csv("data.csv")
 
 with col3:
@@ -27,10 +29,20 @@ with col3:
         st.write(row['description'])
         print(f"images/{row['image']}")
         st.image(f"images/{row['image']}")
-        st.link_button(label='link to web app', url=f"{row['url']}")
+        col3_c1, col3_c2 = st.columns(2)
+        with col3_c1:
+            st.link_button(label='link to web app', url=f"{row['url']}")
+        with col3_c2:
+            st.write(f"[source code]({row['url']})")
+        st.divider()
 with col4:
     for index, row in data[1::2].iterrows():
         st.subheader(row['title'])
         st.write(row['description'])
-        st.image(f"images/{row['image']}")
-        st.link_button(label='link', url=f"{row['url']}")
+        st.image("images/"+row['image'])
+        col4_c1, col4_c2 = st.columns(2)
+        with col4_c1:
+            st.link_button(label='link to web app', url=f"{row['url']}")
+        with col4_c2:
+            st.write(f"[source code]({row['url']})")
+        st.divider()
