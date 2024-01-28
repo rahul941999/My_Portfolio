c4946d3888bcac1ccb25cb6811c2ec532353afcd
rahul941999 <83701790+rahul941999@users.noreply.github.com>
2024-01-28 23:42:51 +0530

    added resume page

--- pages/2_Contact_Me.py
+++ pages/2_Contact_Me.py
@@ -1,14 +1,5 @@
 import streamlit as st
-import Send_email
 
-
-st.title('Contact me :')
-with st.form(key='mail_form'):
-    email_addr = st.text_input("Your E-Mail address: ",
-                               placeholder='xyz@abc.com', key='email')
-    message = st.text_area('Message', height=250,
-                           placeholder='Enter your message', key='msg')
-    button = st.form_submit_button(label='Send')
-    if button:
-        Send_email.send(email_addr, message)
-        st.text("mail sent!")
+st.write("[rahul941999@gmail.com](rahul941999@gmail.com)")
+st.sidebar.markdown('<a href="rahul941999@gmail.com">Contact us !</a>',
+                    unsafe_allow_html=True)
