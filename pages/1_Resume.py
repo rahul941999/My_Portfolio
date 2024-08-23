dd098331580780d6797198a9357b4ff17cbbd167
rahul941999 <83701790+rahul941999@users.noreply.github.com>
2024-08-23 14:50:55 +0530

    updating resume

--- pages/1_Resume.py
+++ pages/1_Resume.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.image("images/resume2024.jpg", width=800)
+st.image("https://drive.google.com/open?id=13lKo6cb5pVEuC7OxE2di_"
+         "dMIhfRt2wmQ&usp=drive_fs", width=800)
