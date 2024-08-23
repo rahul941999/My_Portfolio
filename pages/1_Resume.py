3f15c3acd75e7fe23164203eb5421e2b7b36f421
rahul941999 <83701790+rahul941999@users.noreply.github.com>
2024-08-23 15:03:09 +0530

    get resume from google drive

--- pages/1_Resume.py
+++ pages/1_Resume.py
@@ -1,4 +1,12 @@
+import requests
 import streamlit as st
 
-st.image("https://drive.google.com/file/d/13lKo6cb5pVEuC7OxE2di_dMIhfRt2wmQ/"
-         "view?usp=sharing", width=800)
+# iamge ID
+file_id = "13lKo6cb5pVEuC7OxE2di_dMIhfRt2wmQ"
+
+# URL
+url = f"https://drive.google.com/uc?export=view&id={file_id}"
+
+response = requests.get(url)
+st.image(response.content)
+
