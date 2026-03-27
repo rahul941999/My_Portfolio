f25be37b90aba33da3cebd14c71090a9f4d44738
rahul941999 <83701790+rahul941999@users.noreply.github.com>
2026-03-28 03:24:12 +0530

    get resume from google drive

--- pages/1_Resume.py
+++ pages/1_Resume.py
@@ -1,12 +1,15 @@
 import requests
 import streamlit as st
-
+st.title("Resume")
 # image ID
-file_id = "13lKo6cb5pVEuC7OxE2di_dMIhfRt2wmQ"
-
-# URL
-url = f"https://drive.google.com/uc?export=view&id={file_id}"
-
-response = requests.get(url)
-st.image(response.content)
+# file_id = "13lKo6cb5pVEuC7OxE2di_dMIhfRt2wmQ"
+#
+# # URL
+# url = f"https://drive.google.com/uc?export=view&id={file_id}"
+#
+# response = requests.get(url)
+# st.image(response.content)
+resume = st.file_uploader("../pdf/Rahul_resume.pdf", type="pdf")
+if resume is not None:
+    st.pdf(resume)
 
